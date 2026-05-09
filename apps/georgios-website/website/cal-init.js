6e0b97cbea58884077475dffb94c8182513d2caa
gmour <gmourtzinos1@gmail.com>
2026-05-09 04:11:43 +0100

    feat: Update build workflow and enhance security headers in nginx configuration

---
+++ apps/georgios-website/website/cal-init.js
@@ -0,0 +1,5 @@
+(function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; if(typeof namespace === "string"){cal.ns[namespace] = cal.ns[namespace] || api;p(cal.ns[namespace], ar);p(cal, ["initNamespace", namespace]);} else p(cal, ar); return;} p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
+Cal("init", "30min", {origin:"https://app.cal.com"});
+
+Cal.ns["30min"]("floatingButton", {"calLink":"george-mourtzinos/30min","config":{"layout":"month_view"},"buttonColor":"#e6edf3","buttonTextColor":"#0d1117"});
+Cal.ns["30min"]("ui", {"hideEventTypeDetails":false,"layout":"month_view","cssVarsPerTheme":{"light":{"cal-brand":"#e6edf3"}}});
